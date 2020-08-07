169513e5712b2d5653fb7fc3ae39f70a02288596
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-08-07 17:05:23 +1000

    feat(core): add grid size, make config getter public

--- src/core.rs
+++ src/core.rs
@@ -1,15 +1,17 @@
+use crate::consts::{CONFIG_FILE, GAME_FOLDER};
 use raylib::prelude::*;
 use serde::{Deserialize, Serialize};
-
-const CONFIG_FILE: &str = ".abyss.config.yml";
+use std::fs;
+use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct Core {
     pub rl: RaylibHandle,
     pub thread: RaylibThread,
+    pub config: ProjectConfig,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ProjectConfig {
     project_name: String,
@@ -20,16 +22,9 @@ pub struct ProjectConfig {
 
     #[serde(default = "Core::default_window_height")]
     window_height: i32,
-}
 
-fn get_project_details() -> Result<ProjectConfig, Box<dyn std::error::Error>> {
-    let mut cwd = std::env::current_dir()?;
-    cwd.push(CONFIG_FILE);
-
-    let data = std::fs::read_to_string(cwd).expect("Hello World");
-    let project_info: ProjectConfig = serde_yaml::from_str(&data.as_str()).unwrap();
-
-    Ok(project_info)
+    #[serde(default = "Core::default_grid_size")]
+    pub grid_size: i32,
 }
 
 impl Core {
@@ -41,20 +36,45 @@ impl Core {
         480
     }
 
+    fn default_grid_size() -> i32 {
+        16
+    }
+
+    pub fn get_project_details() -> Result<ProjectConfig, Box<dyn std::error::Error>> {
+        let mut cwd = std::env::current_dir()?;
+        cwd.push(CONFIG_FILE);
+
+        let data = fs::read_to_string(cwd).expect("Hello World");
+        let project_info: ProjectConfig = serde_json::from_str(&data.as_str()).unwrap();
+
+        Ok(project_info)
+    }
+
     pub fn init() -> Core {
-        let project_info = get_project_details().unwrap();
+        let project_info = Core::get_project_details().unwrap();
 
         let (rl, thread) = raylib::init()
             .size(*&project_info.window_width, *&project_info.window_height)
             .title(&project_info.project_name)
             .build();
 
-        Core { rl, thread }
+        Core {
+            rl,
+            thread,
+            config: project_info,
+        }
     }
 
-    pub fn game_loop<State>(&mut self, state: &mut State, f: fn(&mut Core, &mut State)) {
-        while !&self.rl.window_should_close() {
-            f(self, state);
-        }
+    pub fn game_loop<F>(&mut self, cb: F)
+    where
+        F: 'static + Fn(&mut Core),
+    {
+        cb(self);
+    }
+
+    pub fn get_game_folder() -> PathBuf {
+        let mut d = std::env::current_dir().unwrap();
+        d.push(GAME_FOLDER);
+        d
     }
 }
