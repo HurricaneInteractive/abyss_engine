049a9b344abe3b188c52ef07393da8969fe30b7a
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-08-07 17:04:02 +1000

    feat: generic const variables

---
+++ src/consts.rs
@@ -0,0 +1,4 @@
+pub const GAME_FOLDER: &str = ".game";
+pub const MAPPING_FILE: &str = ".abyss.mapping.json";
+pub const SCENE_FOLDER: &str = "scenes";
+pub const CONFIG_FILE: &str = ".abyss.config.json";
